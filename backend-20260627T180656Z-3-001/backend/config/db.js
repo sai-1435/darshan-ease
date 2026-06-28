dbabe82d99c70b1daa001ac72d0f712ecd9947ed
sai-1435 <238x1a0556@khitguntur.ac.in>
2026-06-28 12:28:51 +0530

    Use MongoDB Atlas connection

--- backend-20260627T180656Z-3-001/backend/config/db.js
+++ backend-20260627T180656Z-3-001/backend/config/db.js
@@ -1,8 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/yourdbname", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("✅ Database connected"))
-.catch((err) => console.log("❌ DB Connection Failed:", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Database connected"))
+  .catch((err) => console.log("❌ DB Connection Failed:", err));
